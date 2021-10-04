7ca70b61dbacee047d5cd7ce054218008bb303ad
Peter van Hardenberg <pvh@pvh.ca>
2021-10-04 15:32:15 -0700

    stop overriding the existing git config

--- mergetool-setup.sh
+++ mergetool-setup.sh
@@ -1,1 +1,2 @@
-cp .gitconfig .git/config
+git config --local merge.automerge-driver.name "A custom driver used to merge binary automerge files. (.mrg)"
+git config --local merge.automerge-driver.driver "node merge-automerge.js %A %B"
